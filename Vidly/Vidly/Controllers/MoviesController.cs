8f8dc71ba8007bf6f9b3072913fb4d34ff7234ec
Unknown <mortenolsen4@gmail.com>
2018-10-14 16:55:30 +0200

    Lagde en viewModel klasse

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            
+            var movie = new Movie(){ Name="Shrek!"};
+            var custmers = new List<Customer>
+            {
+                new Customer{ Name="Customer 1"},
+                new Customer{ Name="Customer 2"}
+            };
+
+            //Lager en viewModel for å send ut iRandom viewet
+            var viewModel = new RandomViewModel();
+            viewModel.Movie = movie;
+            viewModel.Customers = custmers;
+
+            return View(viewModel);
+        }
+        [Route("movies/released/{year}/{month}:regex(\\d{2}):range(1,12)")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+    }
+}
